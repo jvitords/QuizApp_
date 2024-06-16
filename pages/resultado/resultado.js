26afff64c61e0cd1cd99eab2d132742fcc256148
João Vitor Santos <jvduarte.santos@hotmail.com>
2024-06-16 00:38:50 +0000

    Adicionado o javascript para a página de resultado

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,41 +1,10 @@
-let tema = "claro"
-import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-
-const body = document.querySelector("body")
-
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")       
-    }
-
-
-
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
-verificarTema()
 verificarTema(body, botaoTema)
